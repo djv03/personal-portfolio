7ec4fd173d7935f9283122d4f879f5904f7e1faf
Dhruvin Vekariya <93385841+djv03@users.noreply.github.com>
2022-12-10 23:29:57 +0530

    dark mode-homepage completed

--- script.js
+++ script.js
@@ -30,23 +30,23 @@ document.querySelector('.hamburger').addEventListener("click",()=>{
 //I want to add slowly blurring effect of the backgorund of the mt image but this is not happening right now so try may be later
 //but try surely
 
-document.querySelector("#mypic1").addEventListener("mouseenter",mouseEnter);
-function mouseEnter() {
-    // console.log("mouse is inside")
-    document.getElementById("mypic1").src = "./myimg2.png";
-  }
+// document.querySelector("#mypic1").addEventListener("mouseenter",mouseEnter);
+// function mouseEnter() {
+//     // console.log("mouse is inside")
+//     document.getElementById("mypic1").src = "./myimg2.png";
+//   }
 
 
 
-document.querySelector("#mypic1").addEventListener("mouseleave",mouseLeave);
-function mouseLeave(){
-    // console.log("mouse outside");
-    document.getElementById("mypic1").src="./IMG_6172.jpg"
-}
+// document.querySelector("#mypic1").addEventListener("mouseleave",mouseLeave);
+// function mouseLeave(){
+//     // console.log("mouse outside");
+//     document.getElementById("mypic1").src="./IMG_6172.jpg";
+// }
 
 
 
-  //js of darkmode------------->
+//js of darkmode------------->
 
   document.querySelector("#click2").style.display="none";
   document.querySelector("#btn").addEventListener('click',()=>{
@@ -63,9 +63,19 @@ function mouseLeave(){
         
 
         document.querySelector(".myinfo").style.color="white";
+        document.querySelector(".ham").style.backgroundColor="royalblue";
+        document.querySelector(".ham").style.border="2px solid royalblue"
+        document.querySelector(".ham").style.borderRadius="7px";
+
+        document.getElementById("mypic1").src="./IMG_6172.jpg";
+
+        document.getElementById("name").style="inline";
+
+        document.querySelector(".mypic img").style="inline";
         
+
        
-   
+        
         
     } else {
         // all of the below things happen when we are turnin light mode
@@ -79,12 +89,19 @@ function mouseLeave(){
     
         document.querySelector("#click1").style.display="inline";
 
-        document.querySelector(".mypic img").style.boxShadow= "0";
-        document.querySelector(".mypic img").style.border= "2px solid red"
-       
+        document.querySelector(".myinfo").style.color="black";
 
-    
-  
+        document.querySelector(".mypic img").style.boxShadow= "0";
+
+        document.getElementById("mypic1").src = "./myimg2.png";
+
+        document.getElementById("name").style.textShadow="0px 0px 0px white";
+        document.getElementById("name").style.color="black";
+
+        document.querySelector(".mypic img").style.boxShadow="0px 0px 0px white";
+        document.querySelector(".mypic img").style.border="5px solid black";
+        
+
     }
   })
 
